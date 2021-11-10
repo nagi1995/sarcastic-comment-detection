de42bd577a75bc41f66254daff2037f79b73ed8d
nagi1995 <binginagesh@gmail.com>
2021-11-10 15:48:18 +0530

    commit to prevent herkou build failure

--- app.py
+++ app.py
@@ -10,14 +10,9 @@ Created on Tue Nov  9 15:18:21 2021
 
 import re
 import pickle
-import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
 from flask import Flask, request, jsonify, render_template
-import requests
-import os
 
 #%%
 
@@ -25,46 +20,11 @@ app = Flask(__name__)
 
 #%%
 
-if os.path.isexists("glove_vectors"):
-    # Reference: https://www.tutorialspoint.com/downloading-files-from-web-using-python
-    
-    url = "https://github.com/nagi1995/sarcastic-comment-detection/blob/main/glove_vectors?raw=true"
-    r = requests.get(url, allow_redirects = True)
-    open('glove_vectors', 'wb').write(r.content)
 
 #%%
-t = pickle.load(open("tokenizer.pkl", 'rb'))
-vocab_size = len(t.word_index) + 1
+vectorizer = pickle.load(open("tfidf-vectorizer.pkl", 'rb'))
+model = pickle.load(open("rf-model.pkl", 'rb'))
 
-glove_model = pickle.load(open("glove_vectors", "rb"))
-glove_words = set(glove_model.keys())
-
-#%%
-max_length = 25
-embedding_matrix = np.zeros((vocab_size, 300)) # vector len of each word is 300
-
-for word, i in t.word_index.items():
-  if word in glove_words:
-    vec = glove_model[word]
-    embedding_matrix[i] = vec
-
-#%%
-tf.keras.backend.clear_session()
-inp = Input(shape = (max_length, ), name = "input")
-embedding = Embedding(input_dim = vocab_size, 
-                      output_dim = 300, # glove vector size
-                      weights = [embedding_matrix], 
-                      trainable = False)(inp)
-lstm = LSTM(32)(embedding)
-flatten = Flatten()(lstm)
-dense = Dense(16, activation = None, 
-              kernel_initializer = "he_uniform")(flatten)
-dropout = Dropout(.25)(dense)
-activation = Activation("relu")(dropout)
-output = Dense(2, activation = "softmax", name = "output")(activation)
-model = Model(inputs = inp, outputs = output)
-model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
-model.load_weights("weights.30-0.9999.hdf5")
 
 #%%
 
@@ -103,9 +63,8 @@ def predict():
         try:
             text = request.form.get("text")
             text = decontracted(text)
-            encoded_text = t.texts_to_sequences([text])
-            padded_text = pad_sequences(encoded_text, maxlen = max_length, padding = "post", truncating = "post")
-            prediction = model.predict(padded_text)
+            x_test = vectorizer.transform([text])
+            prediction = model.predict(x_test)
             prediction_text = "NOT SARCASTIC"
             if prediction[0][1] >= .5:
                 prediction_text = "SARCASTIC"
